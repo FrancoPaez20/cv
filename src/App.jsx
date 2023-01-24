590b35837ec1621438130fa653964121c6f833b2
Franco Paez <franco.p.0109@gmail.com>
2023-01-24 00:07:20 -0300

    Add works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { Works } from "./components/Works";
 
 export const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Works />
     </>
   );
 };
